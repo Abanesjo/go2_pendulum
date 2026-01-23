1feb1fe91611603db2c3e62d0b815724737cde3f
john.abanes@nyu.edu <john.abanes@nyu.edu>
2026-01-22 20:53:33 -0500

    fix usd import

--- source/go2_pendulum/go2_pendulum/tasks/direct/go2_pendulum/go2_pendulum_env_cfg.py
+++ source/go2_pendulum/go2_pendulum/tasks/direct/go2_pendulum/go2_pendulum_env_cfg.py
@@ -6,8 +6,7 @@
 import math
 import os
 
-from isaaclab.actuators.actuator_cfg import ImplicitActuatorCfg
-from isaaclab_assets.robots.unitree import UNITREE_GO2_CFG
+from isaaclab.actuators import ImplicitActuatorCfg
 
 import isaaclab.sim as sim_utils
 from isaaclab.assets import ArticulationCfg
@@ -22,7 +21,49 @@ from isaaclab.sensors import ContactSensorCfg, RayCasterCfg, patterns
 from isaaclab.markers import VisualizationMarkersCfg
 from isaaclab.markers.config import BLUE_ARROW_X_MARKER_CFG, GREEN_ARROW_X_MARKER_CFG
 
-GO2_PENDULUM_USD_PATH = os.path.join(os.path.dirname(__file__), "go2_pendulum", "go2_pendulum.usd")
+GO2_PENDULUM_USD_PATH = os.path.join(os.path.dirname(__file__), "go2_pendulum", "go2_pendulum_robot.usda")
+
+GO2_PENDULUM_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path=GO2_PENDULUM_USD_PATH,
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,
+            solver_position_iteration_count=4,
+            solver_velocity_iteration_count=0,
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.4),
+        joint_pos={
+            ".*L_hip_joint": 0.1,
+            ".*R_hip_joint": -0.1,
+            "F[L,R]_thigh_joint": 0.8,
+            "R[L,R]_thigh_joint": 1.0,
+            ".*_calf_joint": -1.5,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    actuators={
+        "base_legs": ImplicitActuatorCfg(
+            joint_names_expr=[".*_hip_joint", ".*_thigh_joint", ".*_calf_joint"],
+            effort_limit=23.5,
+            velocity_limit=30.0,
+            stiffness=0.0,
+            damping=0.0,
+        ),
+    },
+)
 
 
 @configclass
@@ -124,15 +165,8 @@ class Go2PendulumEnvCfg(DirectRLEnvCfg):
     )
 
     # robot(s)
-    robot_cfg: ArticulationCfg = UNITREE_GO2_CFG.replace(prim_path="/World/envs/env_.*/Robot")
-    robot_cfg.spawn = sim_utils.UsdFileCfg(usd_path=GO2_PENDULUM_USD_PATH)
-    robot_cfg.actuators["base_legs"] = ImplicitActuatorCfg(
-        joint_names_expr=[".*_hip_joint", ".*_thigh_joint", ".*_calf_joint"],
-        effort_limit=23.5,
-        velocity_limit=30.0,
-        stiffness=0.0,
-        damping=0.0,
-    )
+    robot_cfg: ArticulationCfg = GO2_PENDULUM_CFG.replace(prim_path="/World/envs/env_.*/Robot")
+    robot_cfg.articulation_root_prim_path = "/base"
 
     # scene
     scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=4096, env_spacing=4.0, replicate_physics=True)
